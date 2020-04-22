0b61b7981497205fa9a268bdb5de0671b78383d8
gm2552 <gm2552>
2020-04-22 10:46:13 -0500

    Reverting to commit bec0edb3451971629c431752bbadd3245877ca4a.
    Reverting back to pre-m68k refactoring.

--- sources/src/include/memory_uae.h
+++ sources/src/include/memory_uae.h
@@ -62,7 +62,11 @@ extern uae_u32 wait_cpu_cycle_read_ce020 (uaecptr addr, int mode);
 extern void wait_cpu_cycle_write_ce020 (uaecptr addr, int mode, uae_u32 v);
 
 #undef DIRECT_MEMFUNCS_SUCCESSFUL
-#include "machdep/maccess.h"
+//#if !defined(ANDROID) && !defined(ARM)
+//#include "machdep/maccess.h"
+//#else
+#include "machdep/maccess-generic.h"
+//#endif
 
 #include "retrodep/memory_uae.h"
 
@@ -429,3 +433,4 @@ void *arm_memcpy(void *dest, const void *src, size_t n);
 #define uae4all_memcpy memcpy
 #define uae4all_memclr(p,l) memset(p, 0, l)
 #endif
+
